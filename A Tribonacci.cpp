d435dbb662f13f619d582913cc0aaa013c94130c
ridersh2 <79748861+ridersh2@users.noreply.github.com>
2021-07-12 21:55:04 +0700

    .

--- A Tribonacci.cpp
+++ A Tribonacci.cpp
@@ -1,11 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
+int n, i, f[100001], mod = 715827613;
 
 int main() {
+	cin >> n;
+	f[3] = 1;
 
-
+	for(i = 4; i <= n; ++i) f[i] = (f[i - 1] + f[i - 2] + f[i - 3]) % mod;
+	cout << f[n];
 
 	return 0;
 }
